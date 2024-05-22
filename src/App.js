031107e04d4e2c7fea73b92f4edb8b2322383897
Jessica Quirino <qrnojess@gmail.com>
2024-05-22 09:38:41 -0300

    refactor: update project version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import './App.css';
+
 import '../src/assets/css/fonts.css'
 import Router from './routes/router';
 import { createGlobalStyle } from 'styled-components';
- 
-
  
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
     font-family: 'Atkinson Hyperlegible', sans-serif;
-    
-    
+  
   }
 `;
-
-
-
-
 
 
 function App() {
