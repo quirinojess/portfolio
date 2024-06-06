e337400e7b0907641e1507698a144e4f39769a11
Jessica Quirino <qrnojess@gmail.com>
2024-06-06 19:20:39 -0300

    feat: create a headings component

---
+++ src/components/Headings/Headings.tsx
@@ -0,0 +1,17 @@
+import { IHeadings } from "types/Heading";
+import * as S from "./styled";
+
+function Headings({ title, type }: IHeadings) {
+ switch (type) {
+  case "h1":
+   return <S.H1>{title}</S.H1>;
+  case "h2":
+   return <S.H2>{title}</S.H2>;
+  case "h3":
+   return <S.H3>{title}</S.H3>;
+  default:
+   return <S.H4>{title}</S.H4>;
+ }
+}
+
+export { Headings };
--- src/types/Heading.ts
+++ src/types/Heading.ts
@@ -1,7 +1,10 @@
-
-export interface IHeading{
-title: string;
-subtitle: string;
-activeH1: boolean;
-   }
-   
+export interface IHeading {
+   title: string;
+   subtitle: string;
+   activeH1: boolean;
+ }
+ 
+ export interface IHeadings {
+   title: string;
+   type: 'h1' | 'h2' | 'h3' | 'h4';
+ }
