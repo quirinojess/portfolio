25becf3fa5d5f95715ea43f8e41e13bc3d4a936b
Jessica Quirino <qrnojess@gmail.com>
2024-06-12 16:39:12 -0300

    styles: adjusts in margins and sizes

--- src/components/Headings/styled.tsx
+++ src/components/Headings/styled.tsx
@@ -4,7 +4,7 @@ export const H1 = styled.h1`
  color: ${props => props.theme.colors.white};
  font-size: ${props => props.theme.typography.h1};
  font-weight: 800;
- line-height: ${props => props.theme.spacing.large};
+ line-height: ${props => props.theme.spacing.extraLarge};
 `;
 
 export const H2 = styled.h2`
@@ -18,12 +18,12 @@ export const H3 = styled.h3`
  color: ${props => props.theme.colors.primary};
  font-size: ${props => props.theme.typography.h3};
  font-weight: 100;
- line-height: ${props => props.theme.spacing.large};
+ line-height: ${props => props.theme.spacing.medium};
 `;
 
 export const H4 = styled.h4`
  color: ${props => props.theme.colors.secondary};
  font-size: ${props => props.theme.typography.h4};
  font-weight: 700;
- line-height: ${props => props.theme.spacing.large};
+ line-height: ${props => props.theme.spacing.small};
 `;
