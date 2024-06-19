0f5cc7a9c91e62df367996dd7e96760e15301d65
Jessica Quirino <qrnojess@gmail.com>
2024-06-19 15:58:29 -0300

    refactor: adjusts in heading component

--- src/types/Heading.ts
+++ src/types/Heading.ts
@@ -1,9 +1,13 @@
 export interface IHeading {
-   title: string;
-   subtitle: string;
-   activeH1: boolean;
+content: IHeadingContent;
+activeH1?: boolean;
  }
  
+ export interface IHeadingContent {
+  title: string;
+  subtitle: string;
+ }
+
  export interface IHeadings {
    title: string;
    type: 'h1' | 'h2' | 'h3' | 'h4';
