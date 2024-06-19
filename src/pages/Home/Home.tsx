70932603c4a0d1297b8b06ecbc055c9362f2dfd9
Jessica Quirino <qrnojess@gmail.com>
2024-06-19 15:56:37 -0300

    feat: implement sections in home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,9 +6,9 @@ import {
  Headings,
  Figure,
  List,
- Marquee,
  Timeline,
- Social,
+ Sections,
+ Footer,
 } from "components";
 import * as S from "./styled";
 import { HomeContent } from "content";
@@ -16,35 +16,36 @@ import { HomeContent } from "content";
 const Home = () => {
  const testId = "home";
  const {
-  menu,
-  heading,
+  headingHome,
   button,
   description,
   certifieds,
-  skills,
   experience,
-  social,
+  headingAbout,
  } = HomeContent;
 
  return (
   <S.Main data-testid={testId}>
-   <Header content={menu} />
-   <Heading content={heading} />
-   <Paragraph content={description} proportion={"threeFourths"} />
-   <Button content={button} />
-   <S.Section>
+   <Header />
+
+   <Sections flex="column" align="start" justify="start">
+    <Heading content={headingHome} activeH1 />
+    <Paragraph content={description} proportion={"threeFourths"} />
+    <Button content={button} />
+   </Sections>
+
+   <Sections flex="column" align="start" justify="start">
     <Headings title="Experience" type="h2" />
     <Timeline content={experience} />
-   </S.Section>
-   <S.Section>
+   </Sections>
+
+   <Sections flex="row" align="center" justify="space-between">
     <Headings title="Certifieds" type="h2" />
     <List content={certifieds} isModal />
-   </S.Section>
-   <S.Section>
-    <S.AlignTitles>
-     <Headings title="ABOUT" type="h2" />
-     <Headings title="Jessica Quirino" type="h3" />
-    </S.AlignTitles>
+   </Sections>
+
+   <Sections flex="row" align="center" justify="space-between">
+    <Heading content={headingAbout} />
     <Figure
      alt="Serious woman with dark hair and pink blouse"
      src="assets/images/about-me.png"
@@ -53,9 +54,9 @@ const Home = () => {
      height="auto"
     />
     <Paragraph content={description} proportion={"full"} />
-   </S.Section>
-   <Social content={social} />
-   <Marquee content={skills} />
+   </Sections>
+
+   <Footer />
   </S.Main>
  );
 };
