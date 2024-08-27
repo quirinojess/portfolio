653e38fbd0a94c6a52af4c7a6d2958a7eb7f572c
Jessica Quirino <qrnojess@gmail.com>
2024-08-27 06:23:46 -0300

    feat: include new functionalities to paragraph (content rendered wp)

--- src/components/Paragraph/Paragraph.tsx
+++ src/components/Paragraph/Paragraph.tsx
@@ -2,12 +2,12 @@ import { IParagraph } from "types/Paragraph";
 import * as S from "./styled";
 
 function Paragraph({ content, proportion }: IParagraph) {
- const { text } = content;
+ const { text, rendered } = content;
 
  return (
   <S.Paragraph
    proportion={proportion}
-   dangerouslySetInnerHTML={{ __html: text }}
+   dangerouslySetInnerHTML={{ __html: text || rendered || "" }}
   />
  );
 }
