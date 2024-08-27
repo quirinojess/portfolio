a7b0df8b1d1f7b7f2b7951318223509feabbd11a
Jessica Quirino <qrnojess@gmail.com>
2024-08-27 06:28:52 -0300

    feat: load projects from api

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -4,62 +4,58 @@ import { ProjectsContent } from "content";
 import { useActiveSection } from "hooks";
 import { ScrollToTop } from "utils";
 import { useEffect } from "react";
+import { ProjectsService } from "services";
+import { useProjects } from "context/ProjectContext";
 
 const Projects = () => {
  const testId = "projects";
-
  const sectionIds = [`${testId}-head`, `${testId}-projects`];
-
  const activeSection = useActiveSection(sectionIds);
-
  const { headingProjects } = ProjectsContent;
+ const { projects, setProjects } = useProjects();
 
- const mockApi = {
-  projects: [
-   {
-    id: 1,
-    image: {
-     alt: "image",
-     src: "assets/images/thumb-card.png",
-     title: "image",
-     width: 400,
-     height: 400,
-    },
-    title: "Project One",
-    button: { label: "View Full", target: "button-dois" },
-   },
-   {
-    id: 2,
-    image: {
-     alt: "image",
-     src: "assets/images/thumb-card.png",
-     title: "image",
-     width: 400,
-     height: 400,
-    },
-    title: "Project Two",
-    button: { label: "View Full", target: "button-teste" },
-   },
-   {
-    id: 3,
-    image: {
-     alt: "image",
-     src: "assets/images/thumb-card.png",
-     title: "image",
-     width: 400,
-     height: 400,
-    },
-    title: "Project Three",
-    button: { label: "View Full", target: "button-teste" },
-   },
-  ],
- };
+ async function loadProjects() {
+  try {
+   const result = await ProjectsService.getProjects();
+   if (result) {
+    setProjects(result);
+   }
+  } catch (error: any) {
+   console.log("Error loading posts", error);
+  }
+ }
 
- const projectsMap = mockApi.projects.map((project: any) => (
-  <ProjectsCard key={project.id} content={project} />
- ));
+ const projectsMap = projects?.length ? (
+  projects.map((project: any) => (
+   <ProjectsCard
+    key={project.id}
+    content={{
+     id: project.id,
+     image: {
+      alt: project.title.rendered,
+      src: project.fimg_url,
+      title: project.title.rendered,
+      width: "400",
+      height: "400",
+     },
+     title: project.title.rendered,
+     button: {
+      label: "View Full",
+      target: `${project.id}`,
+      ariaLabel: "project-content",
+      ariaExpanded: false,
+      ariaControls: `project-${project.id}`,
+      type: "button",
+     },
+    }}
+   />
+  ))
+ ) : (
+  <p>No projects available.</p>
+ );
 
  useEffect(() => {
+  loadProjects();
   ScrollToTop();
  }, []);
 
