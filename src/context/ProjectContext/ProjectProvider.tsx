2ed24ff17a13f32dde00791951cb131e99a46c8a
Jessica Quirino <qrnojess@gmail.com>
2024-08-27 18:21:47 -0300

    Feat: save projects in local storage

--- src/context/ProjectContext/ProjectProvider.tsx
+++ src/context/ProjectContext/ProjectProvider.tsx
@@ -1,9 +1,18 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import { ProjectsContext } from "./ProjectContext";
 import { TProjectsProvider, TProject } from "types/Project";
 
 const ProjectsProvider = ({ children }: TProjectsProvider) => {
- const [projects, setProjects] = useState<TProject[] | null>(null);
+ const [projects, setProjects] = useState<TProject[] | null>(() => {
+  const savedProjects = localStorage.getItem("projects");
+  return savedProjects ? JSON.parse(savedProjects) : null;
+ });
+
+ useEffect(() => {
+  if (projects !== null) {
+   localStorage.setItem("projects", JSON.stringify(projects));
+  }
+ }, [projects]);
 
  const value = useMemo(() => ({ projects, setProjects }), [projects]);
 
