5502c8867fc332acf71a5f4e1642580700e30a32
Jessica Quirino <qrnojess@gmail.com>
2024-08-27 06:22:43 -0300

    feat: new types for projects

--- src/types/Paragraph.ts
+++ src/types/Paragraph.ts
@@ -1,12 +1,15 @@
-
-export interface IParagraph{
-content: IParagraphContent;
-proportion: IParagraphProportions ;
+export interface IParagraph {
+ content: IParagraphContent;
+ proportion: IParagraphProportions;
 }
-   
-export type IParagraphProportions = 'full' | 'threeFourths' | 'half' | 'oneFourth';
-     
-export interface IParagraphContent{
-text: string;  
- }
-         
+
+export type IParagraphProportions =
+ | "full"
+ | "threeFourths"
+ | "half"
+ | "oneFourth";
+
+export interface IParagraphContent {
+ text?: string;
+ rendered?: string;
+}
