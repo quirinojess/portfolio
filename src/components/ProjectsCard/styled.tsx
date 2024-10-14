d8540a1d427897979e6a632c3c38c46fb0bf6790
Jessica Quirino <qrnojess@gmail.com>
2024-10-14 17:08:59 -0300

    feat: adjusts in filter categorie and project card

--- src/components/ProjectsCard/styled.tsx
+++ src/components/ProjectsCard/styled.tsx
@@ -1,13 +1,32 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { RowBetween } from "themes/CommonAligns";
 
 export const ProjectsCard = styled(RowBetween("div"))`
  flex-wrap: wrap;
+ width: ${props => props.theme.proportions.full};
  max-width: ${props => props.theme.proportions.half};
  gap: ${props => props.theme.spacing.medium} 0;
+ overflow: hidden;
+ position: relative;
+ cursor: pointer;
  img {
   width: ${props => props.theme.proportions.full};
-  height: auto;
+  height: ${props => props.theme.sizes.large};
   object-fit: cover;
+  transition: ${props => props.theme.transitions.short};
  }
+ img:hover {
+  transform: scale(1.1);
+ }
+ ${props => css`
+  @media (max-width: ${props.theme.breakpoints.tablet}) {
+   img {
+    height: ${props.theme.sizes.medium};
+   }
+  }
+  @media (max-width: ${props.theme.breakpoints.mobile}) {
+   max-width: ${props => props.theme.proportions.full};
+   height: ${props.theme.sizes.large};
+  }
+ `}
 `;
