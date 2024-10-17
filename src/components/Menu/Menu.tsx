0019680c5d79b2b08c5aa0af0da1b19270655755
Jessica Quirino <qrnojess@gmail.com>
2024-10-17 09:57:23 -0300

    feat: create mobile menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,14 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { TMenu, TMenuItem } from "types/Menu";
 import * as S from "./styled";
 import { useHandleClick } from "hooks";
 import { useScroll } from "context/ScrollContext";
 
 function Menu(content: TMenu) {
- const testId = "menu";
-
+ const [isOpen, setIsOpen] = useState(false);
  const { handleClick } = useHandleClick();
  const { scrollTarget } = useScroll();
+
+ const testId = "menu";
 
  useEffect(() => {
   if (scrollTarget) {
@@ -19,17 +20,30 @@ function Menu(content: TMenu) {
   }
  }, [scrollTarget]);
 
- const navbarMap = content.content.map((item: TMenuItem) => {
-  return (
-   <S.NavItem
-    key={item.id}
-    onClick={() => handleClick(item.path, item.scroll, false)}
-    onKeyDown={() => handleClick(item.path, item.scroll, false)}>
-    {item.label}
-   </S.NavItem>
-  );
- });
+ const navbarMap = content.content.map((item: TMenuItem) => (
+  <S.NavItem
+   key={item.id}
+   onClick={() => {
+    handleClick(item.path, item.scroll, false);
+    setIsOpen(false);
+   }}
+   onKeyDown={() => handleClick(item.path, item.scroll, false)}>
+   {item.label}
+  </S.NavItem>
+ ));
 
- return <S.Navbar data-testid={testId}>{navbarMap}</S.Navbar>;
+ return (
+  <S.Container>
+   <S.Hamburger onClick={() => setIsOpen(!isOpen)} isOpen={isOpen}>
+    {[...Array(3)].map((_, index) => (
+     <span key={`hamburger-line-${index}`} />
+    ))}
+   </S.Hamburger>
+   <S.Navbar data-testid={testId} isOpen={isOpen}>
+    {navbarMap}
+   </S.Navbar>
+  </S.Container>
+ );
 }
+
 export { Menu };
--- src/components/Menu/styled.tsx
+++ src/components/Menu/styled.tsx
@@ -1,50 +1,105 @@
 import styled, { css, keyframes } from "styled-components";
-import { RowEnd } from "themes/CommonAligns";
+import { RowEnd, ColumnAround } from "themes/CommonAligns";
+
+const openAnimation = keyframes`
+  0% { transform: scale(0.9); }
+  100% { transform: scale(1); }
+`;
 
 const colorChange = keyframes`
   0% { border-color: var(--border-color-primary); }
   50% { border-color: var(--border-color-secondary); }
 `;
 
-export const Navbar = styled(RowEnd("nav"))``;
+export const Container = styled.div`
+ position: relative;
+`;
 
-export const NavItem = styled.button`
- --border-color-primary: ${props => props.theme.colors.secondary};
- --border-color-secondary: ${props => props.theme.colors.primary};
+export const Hamburger = styled(ColumnAround("button"))<{ isOpen: boolean }>`
+ width: ${({ theme }) => theme.sizes.verySmall};
+ height: ${({ theme }) => theme.sizes.verySmall};
+ margin-right: ${({ theme }) => theme.spacing.large};
+ display: none;
  background: none;
  border: none;
  cursor: pointer;
- font-size: ${props => props.theme.typography.medium};
- padding: ${props => props.theme.spacing.small};
- color: ${props => props.theme.colors.white};
- margin: 0 ${props => props.theme.spacing.small};
+ padding: 0;
+ span {
+  width: ${({ theme }) => theme.proportions.full};
+  height: ${({ theme }) => theme.sizes.hiperSmall};
+  background-color: ${({ theme }) => theme.colors.white};
+  transition: ${({ theme }) => theme.transitions.proportionsl};
+ }
+ ${({ isOpen }) =>
+  isOpen &&
+  css`
+   span:nth-child(1) {
+    transform: rotate(45deg) translate(-2px, 10px);
+   }
+   span:nth-child(2) {
+    opacity: 0;
+   }
+   span:nth-child(3) {
+    transform: rotate(-45deg) translate(-3px, -10px);
+   }
+  `}
+ @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+  display: flex;
+ }
+`;
+
+export const Navbar = styled(RowEnd("nav"))<{ isOpen: boolean }>`
+ @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+  background-color: ${({ theme }) => theme.colors.darkDeep};
+  display: ${({ isOpen }) => (isOpen ? "flex" : "none")};
+  margin-top: ${({ theme }) => theme.spacing.large};
+  animation: ${openAnimation} ${({ theme }) => theme.transitions.short};
+  justify-content: center;
+  position: fixed;
+  top: 10;
+  right: 0;
+  left: 0;
+  z-index: 999;
+ }
+ @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+  margin-top: ${({ theme }) => theme.spacing.medium};
+ }
+`;
+
+export const NavItem = styled.button`
+ --border-color-primary: ${({ theme }) => theme.colors.secondary};
+ --border-color-secondary: ${({ theme }) => theme.colors.primary};
+ background: none;
+ border: none;
+ cursor: pointer;
+ font-size: ${({ theme }) => theme.typography.medium};
+ padding: ${({ theme }) => theme.spacing.small};
+ color: ${({ theme }) => theme.colors.white};
+ margin: 0 ${({ theme }) => theme.spacing.small};
  outline: 0;
  position: relative;
  user-select: none;
  touch-action: manipulation;
- ${props => css`
-  @media (max-width: ${props.theme.breakpoints.mobile}) {
-   font-size: ${props => props.theme.typography.small};
-   padding: ${props => props.theme.spacing.verySmall};
-  }
- `}
  &:after {
   content: "";
-  border: 1px solid;
-  border-color: ${props => props.theme.colors.darkDeep};
-  width: 100%;
+  border: ${({ theme }) => theme.borders.thin};
+  border-color: ${({ theme }) => theme.colors.darkDeep};
+  width: ${({ theme }) => theme.proportions.full};
+  height: ${({ theme }) => theme.proportions.full};
+  transition: ${({ theme }) => theme.transitions.short};
   z-index: -1;
   position: absolute;
-  height: 100%;
   top: 7px;
   left: 7px;
-  transition: 0.2s;
  }
-
  &:hover:after {
-  animation: ${colorChange} ${props => props.theme.transitions.medium} linear
+  animation: ${colorChange} ${({ theme }) => theme.transitions.short} linear
    infinite;
-  top: 0px;
-  left: 0px;
+  top: 0;
+  left: 0;
+ }
+ @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+  font-size: ${({ theme }) => theme.typography.small};
+  padding: ${({ theme }) => theme.spacing.verySmall};
  }
 `;
